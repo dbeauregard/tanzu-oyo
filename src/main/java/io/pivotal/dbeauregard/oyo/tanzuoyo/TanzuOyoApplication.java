9070a0e928cdff7d5e74d1c9866fb5663a1170c4
Derek Beauregard <derekbeauregard@MacBook-Pro.localdomain>
2019-11-26 19:20:29 -0700

    Initial import from start.spring.io gerneration.

---
+++ src/main/java/io/pivotal/dbeauregard/oyo/tanzuoyo/TanzuOyoApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.dbeauregard.oyo.tanzuoyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanzuOyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanzuOyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pivotal/dbeauregard/oyo/tanzuoyo/TanzuOyoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.dbeauregard.oyo.tanzuoyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanzuOyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
