970eeb4c2a4e5787abfa0d4a0cf1b4655e77ff26
Derek Beauregard <dbeauregard@vmware.com>
2024-06-17 09:54:24 -0600

    updates for tp4k8s

--- src/main/java/io/pivotal/dbeauregard/oyo/tanzuoyo/Controller.java
+++ src/main/java/io/pivotal/dbeauregard/oyo/tanzuoyo/Controller.java
@@ -1,5 +1,7 @@
 package io.pivotal.dbeauregard.oyo.tanzuoyo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    private static Logger log = LoggerFactory.getLogger(Controller.class);
+
     @GetMapping("/")
     public String defaultPath() {
+        log.info("Default GET method called!  Hello Tanzu!");
         return String.format("Hello %s!\n", "Tanzu Platform");
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("Hello method called with name={}", name);
         return String.format("Hello %s!\n", name);
     }
 
