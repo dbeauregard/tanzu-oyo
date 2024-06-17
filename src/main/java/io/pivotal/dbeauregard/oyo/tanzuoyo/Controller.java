2ed0ff3950285fbe403127cbb41b0a24bec6dfad
Derek Beauregard <dbeauregard@vmware.com>
2024-06-17 09:08:24 -0600

    add default mapping

--- src/main/java/io/pivotal/dbeauregard/oyo/tanzuoyo/Controller.java
+++ src/main/java/io/pivotal/dbeauregard/oyo/tanzuoyo/Controller.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @GetMapping("/")
+    public String defaultPath() {
+        return String.format("Hello %s!\n", "Tanzu Platform");
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!\n", name);
